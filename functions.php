7923d387577ab8c7630534e84827fa2e8a8545ce
Victor Falcon <vfalcon@cleverconsulting.net>
2012-05-18 09:30:50 +0200

    Añadida Function echo_first_image

--- functions.php
+++ functions.php
@@ -1,5 +1,31 @@
 <?php
 /*
+ *  Get First image from post
+ */ 
+function echo_first_image ($postID)
+{					
+	$args = array(
+	'numberposts' => 1,
+	'order'=> 'ASC',
+	'post_mime_type' => 'image',
+	'post_parent' => $postID,
+	'post_status' => null,
+	'post_type' => 'attachment'
+	);
+	
+	$attachments = get_children( $args );
+	
+	if ($attachments) {
+		foreach($attachments as $attachment) {
+			$image_attributes = wp_get_attachment_image_src( $attachment->ID, 'thumbnail' )  ? wp_get_attachment_image_src( $attachment->ID, 'thumbnail' ) : wp_get_attachment_image_src( $attachment->ID, 'full' );
+				
+			echo '<img src="'.wp_get_attachment_thumb_url( $attachment->ID ).'" class="current">';
+			
+		}
+	}
+}
+
+/*
  * Widget: Status Twitter
  */
 include 'includes/twitter.php';
