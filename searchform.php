a0ccf371f65da585a57ba0325900d0eba72b8319
fjuan <fjuan@splendeo.es>
2012-05-28 08:21:13 +0200

    Use of bloginfo('url') instead of the deprecated bloginfo('home')

--- searchform.php
+++ searchform.php
@@ -1,4 +1,4 @@
-<form method="get" id="searchform" action="<?php bloginfo('home'); ?>/">
+<form method="get" id="searchform" action="<?php bloginfo('url'); ?>/">
 <div><input type="text" size="put_a_size_here" name="s" id="s" value="" placeholder="Buscar"/>
 <input type="submit" id="searchsubmit" value="Search" class="btn" />
 </div>
