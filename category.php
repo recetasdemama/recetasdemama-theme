71029c27c498df73362f34eb8c56c45aff5a216e
fjuan <fjuan@splendeo.es>
2012-08-15 11:25:21 +0200

    remove pagination on category pages

--- category.php
+++ category.php
@@ -6,11 +6,17 @@
  * @since Toolbox 0.1
  */
 
-get_header(); ?>
+get_header();
+$wp_query->query_vars["posts_per_page"] = 500;
+$wp_query->get_posts();
+?>
+
 
 		<section id="primary">
 			<div id="content" role="main">
-
+			  
+      <!-- configuracion para evitar la paginacion 	-->
+    
 			<?php if ( have_posts() ) : ?>
 
 				<header class="page-header">
