3671fb989c64a015db8bc2d11a6721a206b9e07a
Francisco de Juan <francisco.juan@gmail.com>
2014-03-16 19:57:09 +0100

    remove unused call to facebook and googleplus JS

--- footer.php
+++ footer.php
@@ -102,15 +102,6 @@ function stickys(){
 		stickys();
 	});
 </script>
-<script type="text/javascript" src="https://apis.google.com/js/plusone.js"></script>
-<div id="fb-root"></div>
-<script>(function(d, s, id) {
-  var js, fjs = d.getElementsByTagName(s)[0];
-  if (d.getElementById(id)) return;
-  js = d.createElement(s); js.id = id;
-  js.src = "//connect.facebook.net/es_ES/all.js#xfbml=1&appId=180884128606005";
-  fjs.parentNode.insertBefore(js, fjs);
-}(document, 'script', 'facebook-jssdk'));</script>
 
 </body>
 </html>
