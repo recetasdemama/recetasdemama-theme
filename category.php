03630e12c03eebf24bb0b00deb01e0ce9ee1fef3
gsmetana <gsmetana@grupoblogsfarm.com>
2014-07-28 15:56:15 +0200

    Cambios para Infinite Scroll y Sprite de imagenes

--- category.php
+++ category.php
@@ -7,6 +7,9 @@
  */
 
 get_header(); ?>
+
+<?php  $paged = (get_query_var('paged')) ? get_query_var('paged') : 1; ?>
+
 <?php if ( is_active_sidebar( 'sidebar-header' ) ) : ?>
 	<div id="sidebar-header" class="widget-area" role="complementary">
 		<?php dynamic_sidebar( 'sidebar-header' ); ?>
@@ -23,7 +26,11 @@ get_header(); ?>
 	$i = 0; // Variable para contar post
 	$est=2; //variable para saber si tengo que poner post entero
 	$iMini = 0; // Variable para contar post
-	if ( have_posts() ) : 
+
+    $cur_cat_id = get_cat_id( single_cat_title("",false) );
+    query_posts( 'cat='. $cur_cat_id .'&posts_per_page=5&paged='. $paged );
+
+    if ( have_posts() ) :
 		/* Start the Loop */ 
 		while ( have_posts() ) : the_post(); 
 				if($i==$est){
@@ -48,18 +55,9 @@ get_header(); ?>
 				//get_template_part('content-list', get_post_format()); 			
 		endwhile;  ?>
 		<div class="clear"></div>
-		<?php toolbox_content_nav( 'nav-below' ); 
-	else : ?>
-        <article id="post-0" class="post no-results not-found">
-            <header class="entry-header">
-                <h1 class="entry-title"><?php _e( 'Nothing Found', 'toolbox' ); ?></h1>
-            </header><!-- .entry-header -->
-      
-            <div class="entry-content">
-                <p><?php _e( 'It seems we can&rsquo;t find what you&rsquo;re looking for. Perhaps searching can help.', 'toolbox' ); ?></p>
-                <?php get_search_form(); ?>
-            </div><!-- .entry-content -->
-        </article><!-- #post-0 -->
-	<?php endif; ?>
+		<?php
+        toolbox_content_nav( 'nav-below' );
+	    endif
+        ?>
 </div><!-- #content -->
 <?php get_footer(); ?>
