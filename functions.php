a1ae3f62670e4f3740b6ff65a967aab9e8fe33b4
Francisco Juan <francisco.juan@gmail.com>
2019-03-06 08:52:58 +0100

    disable emoji support scripts/css via functions.php

--- functions.php
+++ functions.php
@@ -467,3 +467,19 @@ add_action( 'wp_footer', 'rdm_deregister_yarpp_footer_styles' );
 function rdm_deregister_yarpp_footer_styles() {
    wp_dequeue_style('yarppRelatedCss');
 }
+
+/**
+ * Disable the emoji's
+ */
+function disable_emojis() {
+ remove_action( 'wp_head', 'print_emoji_detection_script', 7 );
+ remove_action( 'admin_print_scripts', 'print_emoji_detection_script' );
+ remove_action( 'wp_print_styles', 'print_emoji_styles' );
+ remove_action( 'admin_print_styles', 'print_emoji_styles' );
+ remove_filter( 'the_content_feed', 'wp_staticize_emoji' );
+ remove_filter( 'comment_text_rss', 'wp_staticize_emoji' );
+ remove_filter( 'wp_mail', 'wp_staticize_emoji_for_email' );
+ add_filter( 'tiny_mce_plugins', 'disable_emojis_tinymce' );
+ add_filter( 'wp_resource_hints', 'disable_emojis_remove_dns_prefetch', 10, 2 );
+}
+add_action( 'init', 'disable_emojis' );
